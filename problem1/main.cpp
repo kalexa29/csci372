6b34f80fe9eed7fabfdde3a90ce7531c443ae777
kalexa29 <kalexa29@gmail.com>
2015-01-31 17:30:43 -0500

    Initial Commit

---
+++ problem1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  problem1
+//
+//  Created by Katelynn on 1/31/15.
+//  Copyright (c) 2015 csci. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
